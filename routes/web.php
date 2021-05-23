0e88bdc621f30b030226f79832777d44b6b09f6d
kefer16 <morafi.1999.16@gmail.com>
2021-05-23 15:41:44 -0500

    cambio de ruta profile

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', ['as' => 'home', 'uses' => 'PagesController@home']);
 
-Route::get('servios',['as' => 'servis', 'uses' => ' PagesController@servis']);
+Route::get('servicios',['as' => 'servis', 'uses' => ' PagesController@servis']);
 
 Route::get('contactanos', ['as' => 'contact', 'uses' => 'PagesController@contact']);
 
