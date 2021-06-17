ffb218ad2407fc681c4be6cd5c7bed74503383f9
pedrolujan <lujanmarcelo1203@gmail.com>
2021-06-16 23:41:50 -0500

    Finalizacion de Validacion de formulario

--- public/js/app.js
+++ public/js/app.js
@@ -37345,6 +37345,8 @@ module.exports = function(module) {
 
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
+__webpack_require__(/*! ./contact */ "./resources/js/contact.js");
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
@@ -37389,6 +37391,183 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+/***/ }),
+
+/***/ "./resources/js/contact.js":
+/*!*********************************!*\
+  !*** ./resources/js/contact.js ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var inputs = document.querySelectorAll(".input");
+var Combo = document.querySelectorAll(".cbo");
+var textArea = document.querySelectorAll(".txtArea");
+var expresiones = {
+  usuario: /^[a-zA-Z0-9\_\-]{4,16}$/,
+  // Letras, numeros, guion y guion_bajo
+  nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+  // Letras y espacios, pueden llevar acentos.
+  password: /^.{4,12}$/,
+  // 4 a 12 digitos.
+  correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  telefono: /^\d{7,14}$/,
+  // 7 a 14 numeros.
+  edad: /^\d{2,2}$/
+};
+
+function addclase() {
+  var parent = this.parentNode;
+  parent.classList.add("focus");
+  parent.childNodes[1].classList.add("colorIconos");
+}
+
+function removeclase() {
+  var parent = this.parentNode;
+
+  if (this.value == "" || this.value == 0) {
+    parent.classList.remove("focus");
+    parent.childNodes[1].classList.remove("colorIconos");
+  }
+
+  validarFormulario();
+  validarCombobox();
+} // inputs.forEach(input => {
+// input.addEventListener("focus", addclase);
+// input.addEventListener("blur", removeclase);
+// });
+
+
+textArea.forEach(function (area) {
+  area.addEventListener("focus", addclase);
+  area.addEventListener("keyup", validarFormulario);
+  area.addEventListener("blur", removeclase);
+});
+inputs.forEach(function (input) {
+  input.addEventListener("focus", addclase);
+  input.addEventListener("keyup", validarFormulario);
+  input.addEventListener("blur", removeclase);
+}); // combos.forEach((select) => {
+//     select.addEventListener("change", validarCombobox);
+//     select.addEventListener("blur", validarCombobox);
+// });
+
+Combo.forEach(function (cbo) {
+  cbo.addEventListener("change", validarCombobox);
+  cbo.addEventListener("focus", addclase);
+  cbo.addEventListener("blur", removeclase);
+});
+
+function validarCombobox() {
+  validarCampoTexbox("cboServicio", "Seleccione servicio");
+}
+
+var validarCampoTexbox = function validarCampoTexbox(campo, msj) {
+  if ($("#cboServicio").val() == 0) {
+    document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.add("text-danger");
+    document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.remove("text-success");
+    document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.add("fa-times-circle");
+    document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.remove("fa-check-circle");
+    document.querySelector("#grupo_".concat(campo, " .mensaje")).classList.add("msgError");
+    document.querySelector("#grupo_".concat(campo, " .mensaje")).innerHTML = msj;
+    /*  document.querySelector(`#grupo__${campo} .formulario__grupo-input i`).classList.add('fa-check-circle');
+    document.querySelector(`#grupo__${campo} .formulario__grupo-input i`).classList.remove('fa-times-circle');
+    document.querySelector(`#grupo__${campo} .mensaje`).classList.remove('mensaje-error'); */
+
+    campos[campo] = false;
+  } else {
+    document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.remove("text-danger");
+    document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.add("text-success");
+    document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.add("fa-check-circle");
+    document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.remove("fa-times-circle");
+    document.querySelector("#grupo_".concat(campo, " .mensaje")).innerHTML = "";
+    campos[campo] = true;
+  }
+};
+
+function validarFormulario(e) {
+  switch (e.target.name) {
+    case "txtNombre":
+      validarCampo(expresiones.nombre, e.target, "txtNombre", "Ingrese correctamente sus Nombres");
+      break;
+
+    case "txtApellidos":
+      validarCampo(expresiones.nombre, e.target, "txtApellidos", "Ingrese correctamente sus Apellidos");
+      break;
+
+    case "txtEmail":
+      validarCampo(expresiones.correo, e.target, "txtEmail", "Ingrese correctamente su correo");
+      break;
+
+    case "txtTelefono":
+      validarCampo(expresiones.telefono, e.target, "txtTelefono", "Ingrese correctamente su N° telefono/celular solo numeros");
+      break;
+
+    case "txtRazonSocial":
+      validarCampo(expresiones.nombre, e.target, "txtRazonSocial", "Ingrese correctamente razon social");
+      break;
+
+    case "txtSector":
+      validarCampo(expresiones.nombre, e.target, "txtSector", "Ingrese correctamente Sector");
+      break;
+
+    case "txtMensaje":
+      validarCampo(expresiones.nombre, e.target, "txtMensaje", "Ingrese correctamente Descripcion");
+      break;
+  }
+}
+
+;
+
+var validarCampo = function validarCampo(expresion, input, campo, msj) {
+  // alert(expresion.test(input.value));
+  if (input.value == "") {
+    document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.remove("fa-check-circle");
+    document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.remove("fa-times-circle");
+    document.querySelector("#grupo_".concat(campo, " .mensaje")).innerHTML = "";
+  } else if (expresion.test(input.value)) {
+    // document.getElementById(`${campo}`).classList.add("inputExito");
+    // document.getElementById(`${campo}`).classList.remove("inputErrpor");
+    // document.getElementById(`grupo__${campo}`).classList.remove("datosIncorrectos");
+    // document.getElementById(`grupo__${campo}`).classList.add("datoscorrecto");
+    // document.querySelector(`#grupo__${campo} .container-inputs span`).classList.add("fa-check-circle");
+    // document.querySelector(`#iconos`).classList.add("fa-check-circle");
+    // document.querySelector(`#iconos`).classList.remove("fa-times-circle");
+    if (input.value.length > 2) {
+      document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.remove("text-danger");
+      document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.add("text-success");
+      document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.add("fa-check-circle");
+      document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.remove("fa-times-circle");
+      document.querySelector("#grupo_".concat(campo, " .mensaje")).innerHTML = "";
+      campos[campo] = true;
+    } else {
+      document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.add("text-danger");
+      document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.remove("text-success");
+      document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.add("fa-times-circle");
+      document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.remove("fa-check-circle");
+      document.querySelector("#grupo_".concat(campo, " .mensaje")).classList.add("msgError");
+      document.querySelector("#grupo_".concat(campo, " .mensaje")).innerHTML = msj;
+    } // document.querySelector(`#grupo__${campo} .mensaje`).classList.remove("mensaje-error");
+    //
+
+  } else {
+    // document.getElementById(`${campo}`).classList.add("inputErrpor");
+    // document.getElementById(`${campo}`).classList.remove("inputExito");
+    // document.getElementById(`grupo__${campo}`).classList.add("datosIncorrectos");
+    document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.add("text-danger");
+    document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.remove("text-success");
+    document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.add("fa-times-circle");
+    document.querySelector("#grupo_".concat(campo, " .container-inputs .iconos")).classList.remove("fa-check-circle");
+    document.querySelector("#grupo_".concat(campo, " .mensaje")).classList.add("msgError");
+    document.querySelector("#grupo_".concat(campo, " .mensaje")).innerHTML = msj; // document.querySelector(`#grupo__${campo} .mensaje`).classList.add("mensaje-error");
+    // document.querySelector(`#grupo__${campo} .mensaje`).classList.remove("mensaje-exito");
+    // if ($("#CboPais").val() != 0) {
+    //     document.querySelector(`#grupo__${campo} .mensaje`).innerHTML = msj;
+    // }
+    // campos[campo] = false;
+  }
+};
 
 /***/ }),
 
--- resources/views/form_contact.blade.php
+++ resources/views/form_contact.blade.php
@@ -1,54 +1,110 @@
-<div class="container">
+<div class="container container-form-contact">
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-          <p class="lead">Estamos cerca de ti, en donde te encuentres y a la hora que lo necesites. Con gusto nuestros
+        <p class="lead mb-4  d-none d-md-flex">Estamos cerca de ti, en donde te encuentres y a la hora que lo necesites.
+            Con gusto nuestros
             ejecutivos especializados atenderán tus dudas, recibirán tus comentarios y te brindarán el servicio que
             requieres. Ponemos a tu alcance el medio de comunicación más cómodo para ti.</p>
     </div>
-    <form class="row g-3 mb-5">
-        <div class="col-md-6">
-            <label for="inputEmail4" class="form-label">Nombre</label>
-            <input type="text" class="form-control" id="inputEmail4">
+    <form class="row g-3 mb-5 ml-auto mr-auto col-12 col-xs-12 col-sm-12 col-md-12 col-lg-8">
+        <div id="grupo_txtNombre" class="mt-4 col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <label for="inputEmail4" class="form-label m-0 ">Nombre</label>
+            <div
+                class="container-inputs d-flex p-0 col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 justify-content-center align-items-center">
+                <span class="fas fa-user col-1 col-xs-1 col-sm-1 col-md-1 col-lg-1 justify-content-center"></span>
+                <input type="text" name="txtNombre" class="input d-flex col-10 col-xs-10 col-sm-10 col-md-10 col-lg-10"
+                    id="txtNombre">
+                <span id="iconos" class="fas iconos col-xs-1 col-sm-1 col-md-1 col-lg-1 justify-content-center"></span>
+            </div>
+            <span class="mensaje"></span>
         </div>
-        <div class="col-md-6">
-            <label for="inputPassword4" class="form-label">Apellido</label>
-            <input type="text" class="form-control" id="inputPassword4">
+        <div id="grupo_txtApellidos" class="mt-4 col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <label for="inputEmail4" class="form-label m-0 ">Apellidos</label>
+            <div
+                class="container-inputs d-flex p-0 col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 justify-content-center align-items-center">
+                <span class="fas fa-users col-1 col-xs-1 col-sm-1 col-md-1 col-lg-1 justify-content-center"></span>
+                <input type="text" name="txtApellidos"
+                    class="input ml-1 d-flex col-10 col-xs-10 col-sm-10 col-md-10 col-lg-10" id="txtApellidos">
+                <span id="iconos" class="fas iconos col-xs-1 col-sm-1 col-md-1 col-lg-1 justify-content-center"></span>
+            </div>
+            <span class="mensaje"></span>
         </div>
-        <div class="col-md-12 my-3">
-            <label for="inputEmail4" class="form-label">Correo</label>
-            <input type="email" class="form-control" id="inputEmail4">
+        <div id="grupo_txtEmail" class="mt-4 col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <label for="inputEmail4" class="form-label m-0 ">Email</label>
+            <div class="container-inputs d-flex p-0 col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 align-items-center">
+                <span class="fas fa-envelope col-1 col-xs-1 col-sm-1 col-md-1 col-lg-1 justify-content-center"></span>
+                <input type="text" name="txtEmail" class="input d-flex col-10 col-xs-10 col-sm-10 col-md-10 col-lg-10"
+                    id="txtEmail">
+                <span id="iconos"
+                    class="fas text-md-right iconos col-xs-1 col-sm-1 col-md-1 col-lg-1 justify-content-center"></span>
+            </div>
+            <span class="mensaje"></span>
         </div>
-        <div class="col-md-6 mb-3">
-            <label for="inputPassword4" class="form-label">Teléfono</label>
-            <input type="tel" class="form-control" id="inputPassword4">
+        <div id="grupo_txtTelefono" class="mt-4 col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <label for="inputEmail4" class="form-label m-0 ">Telefono</label>
+            <div
+                class="container-inputs d-flex p-0 col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 justify-content-center align-items-center">
+                <span class="fas fa-phone-alt col-1 col-xs-1 col-sm-1 col-md-1 col-lg-1 justify-content-center"></span>
+                <input type="text" name="txtTelefono"
+                    class="input ml-1 d-flex col-10 col-xs-10 col-sm-10 col-md-10 col-lg-10" id="txtTelefono">
+                <span id="iconos" class="fas iconos col-xs-1 col-sm-1 col-md-1 col-lg-1 justify-content-center"></span>
+            </div>
+            <span class="mensaje"></span>
         </div>
-        <div class="col-md-6 mb-3">
-            <label for="inputEmail4" class="form-label">Razón Social de la Empresa</label>
-            <input type="text" class="form-control" id="inputEmail4">
+        <div id="grupo_txtRazonSocial" class="mt-4 col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <label for="inputEmail4" class="form-label m-0 ">Razón Social de la Empresa</label>
+            <div
+                class="container-inputs d-flex p-0 col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 justify-content-center align-items-center">
+                <span class="fas fa-id-badge col-1 col-xs-1 col-sm-1 col-md-1 col-lg-1 justify-content-center"></span>
+                <input type="text" name="txtRazonSocial"
+                    class="input d-flex col-10 col-xs-10 col-sm-10 col-md-10 col-lg-10" id="txtRazonSocial">
+                <span id="iconos" class="fas iconos col-xs-1 col-sm-1 col-md-1 col-lg-1 justify-content-center"></span>
+            </div>
+            <span class="mensaje"></span>
         </div>
-        <div class="col-md-6">
-            <label for="inputPassword4" class="form-label">Sector</label>
-            <input type="text" class="form-control" id="inputPassword4">
+        <div id="grupo_txtSector" class=" mt-4 col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <label for="inputEmail4" class="form-label  ">Sector</label>
+            <div
+                class="container-inputs d-flex p-0 col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 justify-content-center align-items-center">
+                <span class="fas fa-chart-area col-1 col-xs-1 col-sm-1 col-md-1 col-lg-1 justify-content-center"></span>
+                <input type="text" name="txtSector" class="input d-flex col-10 col-xs-10 col-sm-10 col-md-10 col-lg-10"
+                    id="txtSector">
+                <span id="iconos" class="fas iconos col-xs-1 col-sm-1 col-md-1 col-lg-1 justify-content-center"></span>
+            </div>
+            <span class="mensaje"></span>
         </div>
 
-        <div class=" col-md-6 form-group">
-            <label for="exampleFormControlSelect1">¿Qué Nesecita?</label>
-            <select class="form-control" id="exampleFormControlSelect1">
-              <option>Auditoría e Impuestos</option>
-              <option>Contabilidad & Finanzas</option>
-              <option>Legal & Laboral</option>
-              <option>Prevención de Lavado de Activos</option>
-              <option>Eventos y Capacitaciones</option>
-            </select>
+        <div id="grupo_cboServicio" class=" mt-4 col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <label for="exampleFormControlSelect1" class="">¿Qué Nesecita?</label>
+            <div
+                class="container-inputs d-flex p-0 col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 justify-content-center align-items-center">
+                <span class="fas fa-poll-h col-1 col-xs-1 col-sm-1 col-md-1 col-lg-1 justify-content-center"></span>
+                <select name="cboServicio" class="cbo  col-10 col-xs-10 col-sm-10 col-md-10 col-lg-10" id="cboServicio">
+                    <option value="0">Seleccione Opcion</option>
+                    <option value="1">Auditoría e Impuestos</option>
+                    <option value="2">Contabilidad & Finanzas</option>
+                    <option value="3">Legal & Laboral</option>
+                    <option value="4">Prevención de Lavado de Activos</option>
+                    <option value="5">Eventos y Capacitaciones</option>
+                </select>
+                <span id="iconos"
+                    class="fas text-md-right iconos col-xs-1 col-sm-1 col-md-1 col-lg-1 justify-content-center"></span>
+            </div>
+            <span class="mensaje"></span>
         </div>
-        <div class="col-md-12">
-            <label for="validationTextarea">Mensaje</label>
-            <textarea class="form-control" placeholder="Describa el mensaje" required></textarea>
+        <div id="grupo_txtMensaje" class=" mt-4 col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <label for="inputEmail4" class="m-0 ">Mensaje:</label>
+            <div
+                class="container-inputs d-flex p-0 col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 justify-content-center align-items-center">
+                <span
+                    class="fas fa-comment-alt col-1 col-xs-1 col-sm-1 col-md-1 col-lg-1 justify-content-center"></span>
+                <textarea name="txtMensaje" class="txtArea col-10 col-xs-10 col-sm-10 col-md-10 col-lg-10"  placeholder="Describa el mensaje" id="txtMensaje" required></textarea>
+                <span id="iconos" class="fas text-md-right iconos col-xs-1 col-sm-1 col-md-1 col-lg-1 justify-content-center"></span>
+            </div>
+            <span class="mensaje"></span>
         </div>
-        <div class="col-12 mt-4">
-            <button type="submit" class="btn btn-primary">Enviar</button>
-        </div>
-        <div class="col-12 mt-4">
-            <button type="submit" class="btn btn-primary">Enviar prueba</button>
+        <div class="text-center  ml-auto mr-auto mt-4 col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <button type="submit"
+                class="btn btn-primary p-2 col-11 col-xs-11 col-sm-11 col-md-11 col-lg-11">Enviar</button>
         </div>
 
     </form>
